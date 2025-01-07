2cf9e89689e5c74c4ab187bafab39f252da324cc
PGross <paul.riedlingen@gmail.com>
2025-01-07 15:42:22 +0100

    added basic consumer producer creation

--- src/include/duckdb/optimizer/factorization_optimizer.hpp
+++ src/include/duckdb/optimizer/factorization_optimizer.hpp
@@ -8,11 +8,44 @@
 
 #pragma once
 
+#include <utility>
+
 #include "duckdb/optimizer/rule.hpp"
 #include "duckdb/planner/logical_operator_visitor.hpp"
 #include "duckdb/common/types/value.hpp"
 
 namespace duckdb {
+
+
+struct FactorConsumer {
+	explicit FactorConsumer(vector<ColumnBinding> flat_columns) : flat_columns(std::move(flat_columns)) {
+	}
+
+	void Print() const {
+		Printer::Print("FactorConsumer");
+		for (auto &column : flat_columns) {
+			Printer::Print(StringUtil::Format("ColumnIndex: %llu, TableIndex: %llu", column.column_index, column.table_index));
+		}
+	}
+
+	//! The columns that need to be flat, e.g. aggregate keys can be inside the factor
+	vector<ColumnBinding> flat_columns;
+};
+
+struct FactorProducer {
+	explicit FactorProducer(vector<ColumnBinding> factor_columns) : factor_columns(std::move(factor_columns)) {
+	}
+
+	void Print() const {
+		Printer::Print("FactorProducer");
+		for (auto &column : factor_columns) {
+			Printer::Print(StringUtil::Format("ColumnIndex: %llu, TableIndex: %llu", column.column_index, column.table_index));
+		}
+	}
+
+	//! The columns that are factorized
+	vector<ColumnBinding> factor_columns;
+};
 
 //! Todo: Add a description
 class FactorizationOptimizer : public LogicalOperatorVisitor {
@@ -21,5 +54,13 @@ public:
 
 public:
 	void VisitOperator(LogicalOperator &op) override;
+
+private:
+	static bool CanProduceFactors(LogicalOperator &op);
+	static FactorProducer GetFactorProducer(LogicalOperator &op);
+
+	static bool CanConsumeFactors(LogicalOperator &op);
+	static FactorConsumer GetFactorConsumer(LogicalOperator &op);
 };
+
 } // namespace duckdb
--- src/optimizer/factorization_optimizer.cpp
+++ src/optimizer/factorization_optimizer.cpp
@@ -6,6 +6,121 @@ FactorizationOptimizer::FactorizationOptimizer() {
 }
 
 void FactorizationOptimizer::VisitOperator(LogicalOperator &op) {
+	auto column_bindings = op.GetColumnBindings();
+
+	if (CanConsumeFactors(op)) {
+		const auto consumer = GetFactorConsumer(op);
+		consumer.Print();
+	}
+
+
+	if (CanProduceFactors(op)) {
+		const auto producer = GetFactorProducer(op);
+		producer.Print();
+	}
+
+	for (auto &child : op.children) {
+		VisitOperator(*child);
+	}
+}
+
+bool FactorizationOptimizer::CanProduceFactors(LogicalOperator &op) {
+	switch (op.type) {
+	case LogicalOperatorType::LOGICAL_COMPARISON_JOIN: {
+		const JoinType join_type = op.Cast<LogicalComparisonJoin>().join_type;
+		switch (join_type) {
+		case JoinType::MARK:
+		case JoinType::SEMI:
+		case JoinType::ANTI: {
+			return false;
+		}
+		default: {
+			return true;
+		}
+		}
+	}
+	default:
+		return false;
+	}
+}
+
+FactorProducer FactorizationOptimizer::GetFactorProducer(LogicalOperator &op) {
+	switch (op.type) {
+	case LogicalOperatorType::LOGICAL_COMPARISON_JOIN: {
+		const auto &comparison_join = op.Cast<LogicalComparisonJoin>();
+		const auto rhs_bindings = comparison_join.children[1]->GetColumnBindings();
+		return FactorProducer(rhs_bindings);
+	}
+	default: {
+		throw NotImplementedException("This operator cannot produce factors");
+	}
+	}
+}
+
+bool FactorizationOptimizer::CanConsumeFactors(LogicalOperator &op) {
+	switch (op.type) {
+	case LogicalOperatorType::LOGICAL_AGGREGATE_AND_GROUP_BY:
+		return true;
+	default:
+		return false;
+	}
+}
+
+void GetColumnBindingOfExpression(const unique_ptr<Expression> &expression, vector<ColumnBinding> &collector) {
+	switch (expression->GetExpressionClass()) {
+	case ExpressionClass::BOUND_CONSTANT:
+		break; // nothing to do for this expression
+	case ExpressionClass::BOUND_COLUMN_REF: {
+		const auto &column_ref = expression->Cast<BoundColumnRefExpression>();
+		collector.push_back(column_ref.binding);
+		break;
+	}
+	case ExpressionClass::BOUND_CONJUNCTION: {
+		const auto &conjunction = expression->Cast<BoundConjunctionExpression>();
+		for (auto &child : conjunction.children) {
+			GetColumnBindingOfExpression(child, collector);
+		}
+		break;
+	}
+	case ExpressionClass::BOUND_FUNCTION: {
+		const auto &function = expression->Cast<BoundFunctionExpression>();
+		for (auto &child : function.children) {
+			GetColumnBindingOfExpression(child, collector);
+		}
+		break;
+	}
+	case ExpressionClass::BOUND_COMPARISON: {
+		const auto &comparison = expression->Cast<BoundComparisonExpression>();
+		GetColumnBindingOfExpression(comparison.left, collector);
+		GetColumnBindingOfExpression(comparison.right, collector);
+		break;
+	}
+	default:
+		const auto class_name = ExpressionClassToString(expression->GetExpressionClass());
+		printf("Expression class %s not supported\n", class_name.c_str());
+		break;
+	}
+}
+
+FactorConsumer FactorizationOptimizer::GetFactorConsumer(LogicalOperator &op) {
+	switch (op.type) {
+	case LogicalOperatorType::LOGICAL_AGGREGATE_AND_GROUP_BY: {
+
+		const auto &aggregate = op.Cast<LogicalAggregate>();
+		const auto &group_expressions = aggregate.groups;
+
+		vector<ColumnBinding> group_expression_bindings = {};
+
+		for (auto &expression : group_expressions) {
+			GetColumnBindingOfExpression(expression, group_expression_bindings);
+		}
+
+		return FactorConsumer(group_expression_bindings);
+	}
+	default: {
+		throw NotImplementedException("This operator cannot consume factors");
+	}
+	}
 }
 
 } // namespace duckdb
